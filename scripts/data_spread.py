28d7052b144ec32cc2ef43650d2e63b8b9f89e46
Noah Gardner <ngngardner@gmail.com>
2021-08-26 19:49:22 -0400

    add todo

--- scripts/data_spread.py
+++ scripts/data_spread.py
@@ -2,13 +2,8 @@
 import numpy as np
 
 if __name__ == "__main__":
-    import cuticle_analysis
-    print(cuticle_analysis)
-
-    # for debugging
-    # from cuticle_analysis.core import init
-    # init()
-
+    # TODO: add AllSub, RoughSmoothFull, and AllFull
+    # TODO: add class function for representing data spread
     from cuticle_analysis.datasets import RoughSmoothSub  # noqa
 
     data = RoughSmoothSub((16, 16), save=True, rebuild=True)
