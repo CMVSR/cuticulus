593508d11e46bf0da2a4ffc430cf0b4158844241
Noah Gardner <ngngardner@gmail.com>
2021-10-17 16:33:14 -0400

    cleanup default.nix

--- default.nix
+++ default.nix
@@ -1,3 +1,31 @@
-(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
-  src = ./.;
-}).defaultNix
+{ lib, python39Packages, gitignoreSource }:
+
+
+
+python39Packages.buildPythonPackage
+{
+  pname = "cuticle_analysis";
+  version = "0.0.1";
+
+  src = gitignoreSource ./.;
+
+  propagatedBuildInputs = with python39Packages; [
+    colorama
+    gdown
+    matplotlib
+    numpy
+    opencv3
+    openpyxl
+    pandas
+    pillow
+    python-dotenv
+    requests
+    scikit-learn
+    scipy
+    tensorflow
+    questionary
+  ];
+
+  doCheck = false;
+  pythonImportsCheck = [ "cuticle_analysis" ];
+}
--- flake.nix
+++ flake.nix
@@ -5,69 +5,35 @@
     nixpkgs.url = "github:NixOS/nixpkgs/d189bf92f9be23f9b0f6c444f6ae29351bb7125c";
     utils = { url = "github:numtide/flake-utils"; };
     configs = { url = "github:ngngardner/configs"; };
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, configs }:
-    utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils, configs, gitignore }:
+    {
+      overlay = final: prev: {
+        cuticle = final.callPackage ./default.nix { };
+        inherit (gitignore.lib) gitignoreSource;
+      };
+    } //
+    (utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
             configs.overlay
+            self.overlay
           ];
-        };
-
-        cuticle = pkgs.python39Packages.buildPythonPackage {
-          pname = "cuticle_analysis";
-          version = "0.0.1";
-
-          src = pkgs.lib.cleanSourceWith {
-            filter = (path: type:
-              ! (builtins.any
-                (r: (builtins.match r (builtins.baseNameOf path)) != null)
-                [
-                  "dataset"
-                  "logs"
-                  "result"
-                  "pip_packages"
-                  "scripts"
-                  "gui"
-                  "paper"
-                  ".*\.egg-info"
-                  ".*\.zip"
-                ])
-            );
-            src = pkgs.lib.cleanSource ./.;
-          };
-
-          propagatedBuildInputs = with pkgs.python39Packages; [
-            colorama
-            gdown
-            matplotlib
-            numpy
-            opencv3
-            openpyxl
-            pandas
-            pillow
-            python-dotenv
-            requests
-            scikit-learn
-            scipy
-            tensorflow
-            questionary
-          ];
-
-          doCheck = false;
-          pythonImportsCheck = [ "cuticle_analysis" ];
         };
       in
       {
-        packages.cuticle = cuticle;
-        defaultPackage = cuticle;
+        defaultPackage = pkgs.callPackage ./default.nix { };
 
         devShell = pkgs.mkShell {
           packages = [
-            cuticle
+            pkgs.cuticle
 
             # python dev
             pkgs.python39Packages.autopep8
@@ -80,10 +46,12 @@
             pkgs.python39Packages.pygame
 
             # paper dev
-            pkgs.texlive.combined.scheme-small
-            pkgs.jabref
+            (pkgs.texlive.combine {
+              inherit (pkgs.texlive) scheme-small;
+            })
             pkgs.pylatex
           ];
         };
-      });
+      })
+    );
 }
