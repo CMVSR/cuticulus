8908a22c6f77c18e6da3b7769db937054466fbc8
theTechOwl <theTechOwl@users.noreply.github.com>
2021-12-05 17:28:45 -0500

    Fixed an error where if the taxon dict contains a key value of "subspecies" and not "sub-species", the program crashes.

--- gui/objects/panes/species_pane.py
+++ gui/objects/panes/species_pane.py
@@ -10,16 +10,17 @@ class SpeciesPane(Pane):
         super().__init__(surface, position, title, pane_size)
         self.set_species_taxon(taxon_dict)
         self._p_font = pygame.font.SysFont('Arial', 16)
+
+    def _reset_species_taxon(self, index):
+        "Resets the values for the species taxon."
+        keys = list(self._species_taxon.keys())
+        self._species_taxon[keys[index]] = None
+        if index > 0:
+            self._reset_species_taxon(index - 1)
         
-    def set_species_taxon(self, hierarchy, h_name):
-        """Sets the value of a species taxonomy."""
-        keys = self._species_taxon.keys()
-        if hierarchy.lower() in keys:
-            self._species_taxon[hierarchy] = h_name
-        else:
-            raise KeyError(hierarchy + " is not a valid taxon hierarchy.")
 
     def _set_species_taxon(self, taxon_dict, dict_ind):
+        "Recursively sets the species taxon values."
         if dict_ind > 0:
             self._set_species_taxon(taxon_dict, dict_ind - 1)
         key_name = list(taxon_dict.keys())[dict_ind]
@@ -27,31 +28,47 @@ class SpeciesPane(Pane):
         if key_name in self_keys:
             self._species_taxon[key_name] = taxon_dict[key_name]
         else:
-            raise KeyError(key_name + " is not a valid taxon hierarchy.")
+            if (key_name == 'subspecies') & ('sub-species' in self_keys):
+                self._species_taxon["sub-species"] = taxon_dict['subspecies']
+            else:
+                raise KeyError(key_name + " is not a valid taxon hierarchy.")
 
     def set_species_taxon(self, taxon_dict):
         """Sets the value of a species taxonomy."""
+        self._reset_species_taxon(len(self._species_taxon)-1)
         self._set_species_taxon(taxon_dict, len(taxon_dict.keys()) - 1)
 
-    def _append_panes(self, h_key_str, h_val_str, delimiter, index, h_key_list, h_val_list):
+    def _append_texts(self, h_key_str, h_val_str, delimiter, index, h_key_list, h_val_list):
+        """
+            Appends species keys and values text objects into their corresponding lists.
+        """
         if index > 0:
-            self._append_panes(h_key_str, h_val_str, delimiter, index-1, h_key_list, h_val_list)
-        self._append_pane(h_key_str, h_val_str, delimiter, index, h_key_list, h_val_list)
+            self._append_texts(h_key_str, h_val_str, delimiter, index-1, h_key_list, h_val_list)
+        self._append_text(h_key_str, h_val_str, delimiter, index, h_key_list, h_val_list)
         
 
-    def _append_pane(self, h_key_str, h_val_str, delimiter, index, h_key_list = [], h_val_list = []):
+    def _append_text(self, h_key_str, h_val_str, delimiter, index, h_key_list = [], h_val_list = []):
+        """
+            See _append_text().
+        """
         h_key_split = h_key_str.split(delimiter)
         h_val_split = h_val_str.split(delimiter)
         h_key_list.append(self._p_font.render(h_key_split[index], True, PANE_BD_KEY_CLR))
         h_val_list.append(self._p_font.render(h_val_split[index], True, DFLT_FNT_CLR))
     
-    def _render_pane(self, h_key_list, h_val_list, index):
+    def _render_text(self, h_key_list, h_val_list, index):
+        """
+            Recursive method of rendering species info text into GUI.
+        """
         if index > 0:
-            self._render_pane(h_key_list, h_val_list, index-1)
+            self._render_text(h_key_list, h_val_list, index-1)
         self.surface.blit(h_key_list[index], (self._content_attrib['position'][0], self._content_attrib['position'][1] + (index * 16)))
         self.surface.blit(h_val_list[index], ((self._content_attrib['position'][0] + 120), self._content_attrib['position'][1] + (index * 16)))
 
-    def _render_panes(self, h_key_str, h_val_str):
+    def _render_texts(self, h_key_str, h_val_str):
+        """
+            Renders the species info into the gui.
+        """
         h_key_split = h_key_str.split("\n")
         h_val_split = h_val_str.split("\n")
         if len(h_key_split) != len(h_val_split):
@@ -59,8 +76,8 @@ class SpeciesPane(Pane):
         index = len(h_key_split) - 1
         h_key_list = []
         h_val_list = []
-        self._append_panes(h_key_str, h_val_str, '\n', index, h_key_list, h_val_list)
-        self._render_pane(h_key_list, h_val_list, len(h_key_list)-1)
+        self._append_texts(h_key_str, h_val_str, '\n', index, h_key_list, h_val_list)
+        self._render_text(h_key_list, h_val_list, len(h_key_list)-1)
 
         
 
@@ -95,8 +112,4 @@ class SpeciesPane(Pane):
         self._content_attrib = super().show()
         h_key_str = self.to_string(True)
         h_val_str = self.to_string(False)
-        self._render_panes(h_key_str, h_val_str)
-        #h_key_text = self._p_font.render(h_key_str, True, PANE_BD_KEY_CLR)
-        #h_val_text = self._p_font.render(h_val_str, True, DFLT_FNT_CLR)
-        #self.surface.blit(h_key_text, (self._content_attrib['position'][0], self._content_attrib['position'][1]))
-        #self.surface.blit(h_val_text, ((self._content_attrib['position'][0]+ 50), self._content_attrib['position'][1]))
+        self._render_texts(h_key_str, h_val_str)
