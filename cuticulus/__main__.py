9cf1e2dbd6a4a40cf9ffbd2dc6d6c639fba25d31
Noah Gardner <ngngardner@gmail.com>
2022-09-23 11:21:09 -0400

    remove sanity check

--- cuticulus/__main__.py
+++ cuticulus/__main__.py
@@ -3,11 +3,8 @@ import shutil
 from pathlib import Path
 
 from dotenv import load_dotenv
-from juliacall import Main as jl
 
 from cuticulus.datasets import RoughSmoothFull
-
-jl.seval('using Pkg; Pkg.instantiate(); Pkg.resolve()')
 
 
 def main():
