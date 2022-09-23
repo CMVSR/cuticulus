92cc992febad5f4e0e579e81ff19c480417b388e
Noah Gardner <ngngardner@gmail.com>
2022-09-22 20:58:56 -0400

    refactor downloader,

--- cuticulus/core/datasets/downloader.py
+++ cuticulus/core/datasets/downloader.py
@@ -1,14 +1,10 @@
 """Download raw dataset."""
-
 import logging
 from pathlib import Path
-from zipfile import PyZipFile
 
-import gdown
-import questionary
+from juliacall import Main as jl
 
 from cuticulus import const
-from cuticulus.console import console
 
 log = logging.getLogger('rich')
 
@@ -17,48 +13,33 @@ class Downloader(object):
     """Download raw dataset."""
 
     def __init__(self):
-        """Initialize Download class."""
-        self.raw = Path.cwd() / 'dataset.zip'
-        self.base_path = Path.cwd() / 'dataset'
-        self.download()
-
-    def download_zip(self):
-        """Download raw dataset .zip file."""
-        if not self.raw.exists():
-            console.log('Dataset .zip not found.')
-
-            prompt = 'Okay to download dataset to: "{0}"?'.format(self.raw)
-            if questionary.confirm(prompt).ask():
-                log.info('Downloading dataset...')
-                gdown.download(const.DATASET_URL, str(self.raw))
-                log.info('Downloaded dataset!')
-            else:
-                console.log('Download cancelled.')
-
-    def unzip_dataset(self):
-        """Unzip dataset to the proper path."""
-        if not self.base_path.exists():
-            console.log('Dataset directory not found.')
-
-            prompt = 'Okay to unzip dataset to: "{0}"?'.format(self.base_path)
-            if questionary.confirm(prompt).ask():
-                log.info('Extracting dataset...')
-                PyZipFile(self.raw).extractall(str(self.base_path))
-                log.info('Extracted dataset!')
-            else:
-                console.log('Extraction cancelled.')
-
-    def download(self):
-        """Download dataset.
-
-        Raises:
-            FileNotFoundError: cannot find zip or dataset
+        """Initialize downloader."""
+        name = 'cuticulus-dataset-raw'
+        description = """
+        Dataset: Cuticle texture ant head image dataset (raw)
+        License: MIT
+        Website: {website}
         """
-        self.download_zip()
-        self.unzip_dataset()
+        description = description.format(website=const.DATASET_URL)
 
-        if not self.raw.exists():
-            raise FileNotFoundError
+        register = """
+        register(DataDep(
+            "{name}",
+            "{description}",
+            "{url}",
+            "{hash}",
+            post_fetch_method = unpack,
+        ))
+        """
+        register = register.format(
+            name=name,
+            description=description,
+            url=const.DATASET_URL,
+            hash=const.DATASET_HASH,
+        )
+        # remove new lines
+        register = register.replace('\n', '')
 
-        if not self.base_path.exists():
-            raise FileNotFoundError
+        jl.seval('using DataDeps')
+        jl.seval(register)
+        self.base_path = Path(jl.seval('datadep"{name}"'.format(name=name)))
